360cb1a6d317096e7f8b6a5693a01da69c1976af
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-25 17:44:26 +0530

    UC1CreateStack

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StackAndQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stacks stack = new Stacks();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
